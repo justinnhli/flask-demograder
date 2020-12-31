326cbc7488ed45fdd5e4428abcae1f275b939d05
Justin Li <justinnhli@gmail.com>
2020-12-30 16:20:34 -0800

    prettify code

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ from pathlib import Path
 
 from flask import Flask
 
-from .models import db
 from .auth import oauth, blueprint as auth_blueprint
-from .routes import blueprint
+from .models import db
+from .routes import blueprint as routes_blueprint
 
 
 def create_app():
+    # create app
     app = Flask(
         __name__,
         root_path=Path(__file__).expanduser().resolve().parent,
     )
+    # configure app
     app.config.from_pyfile('settings.py')
     app.secret_key = app.config['FLASK_SECRET_KEY']
+    # initialize extensions
     db.init_app(app)
     oauth.init_app(app)
     oauth.register(
@@ -23,7 +26,8 @@ def create_app():
             'scope': 'openid email profile',
         }
     )
-
-    app.register_blueprint(blueprint)
+    # register blueprints
+    app.register_blueprint(routes_blueprint)
     app.register_blueprint(auth_blueprint)
+    # return
     return app
