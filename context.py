d183b93ad6e0773b3a6d0e081caa26fa79640a0a
Justin Li <justinnhli@gmail.com>
2022-03-06 16:46:12 -0800

    refactor get_context()

--- context.py
+++ context.py
@@ -2,6 +2,8 @@ from enum import IntEnum
 
 from flask import session, request, abort
 
+from .models import User, Instructor, Student, Course
+
 
 class Role(IntEnum):
     '''A class representing the role of the viewer.
@@ -14,62 +16,95 @@ class Role(IntEnum):
     ADMIN = 3
 
 
-def get_context(**kwargs):
-    user = User.query.filter_by(email=session.get('user_email')).first()
-    context = {}
-    context['user'] = user
-    # check if login is required
-    if not kwargs.get('login_required', True):
-        return context
-    elif not user:
-        abort(401)
-    # check if the user is an instructor of the course
+def _set_user_context(context, url_args, **kwargs):
+    context['user'] = User.query.filter_by(email=session.get('user_email')).first()
+
+
+def _set_viewer_context(context, url_args, **kwargs):
+    if context['user'].admin and 'viewer' in url_args:
+        context['viewer'] = User.query.filter_by(email=url_args['viewer']).first()
+    if not context.get('viewer', None):
+        context['viewer'] = context['user']
+    context['alternate_view'] = (context['user'] != context['viewer'])
+
+
+def _set_course_context(context, url_args, **kwargs):
+    # TODO determine question, assignment, and course
+    # TODO check course access permissions
+    if 'course_id' in kwargs:
+        context['course'] = Course.query.filter_by(id=kwargs['course_id']).first()
+
+
+def _set_instructor_context(context, url_args, **kwargs):
     if 'course' in context:
         context['instructor'] = True # FIXME
     else:
         context['instructor'] = False
-    # get URL parameters
-    args = request.args.to_dict()
-    # Set admin rendering parameters, viewer and role.
-    # These two parameters overlap in semantics slightly. The viewer parameter
-    # is used to render a page as though they are the user. This is helpful for
-    # checking that a page renders correctly. However, an appropriate viewer
-    # is not always available; for example, if an admin is the sole instructor
-    # of a course, and wants to see the page as a non-admin faculty, there is
-    # no viewer that could provide that. Hence the role parameter, which can 
-    # restrict the privileges of the user. In sum:
-    # * the user is for checking actual permissions
-    # * the viewer is for acting as a specific person
-    # * the role is for checking renders
-    # set viewer
-    if user.admin and 'viewer' in args:
-        context['viewer'] = User.query.filter_by(email=args['viewer']).first()
-    if not context.get('viewer', None):
-        context['viewer'] = user
-    context['alternate_view'] = (context['user'] != context['viewer'])
-    # set role
+
+
+def _set_role_context(context, url_args, **kwargs):
     context['Role'] = Role # this allows templates to branch on role
-    if 'role' in args and args['role'].upper() not in Role.__members__.keys():
-        del args['role']
+    if 'role' in url_args and url_args['role'].upper() not in Role.__members__:
+        del url_args['role']
     if context['viewer'].admin:
         context['role'] = min(
-            Role[args.get('role', 'admin').upper()],
+            Role[url_args.get('role', 'admin').upper()],
             Role.ADMIN,
         )
         context['alternate_view'] = context['alternate_view'] or (context['role'] != Role.ADMIN)
     elif context['viewer'].faculty:
         context['role'] = min(
-            Role[args.get('role', 'faculty').upper()],
+            Role[url_args.get('role', 'faculty').upper()],
             Role.FACULTY,
         )
         context['alternate_view'] = context['alternate_view'] or (context['role'] != Role.FACULTY)
     elif context['instructor']:
         context['role'] = min(
-            Role[args.get('role', 'instructor').upper()],
+            Role[url_args.get('role', 'instructor').upper()],
             Role.INSTRUCTOR,
         )
         context['alternate_view'] = context['alternate_view'] or (context['role'] != Role.INSTRUCTOR)
     else:
         context['role'] = Role.STUDENT
         context['alternate_view'] = context['alternate_view'] or (context['role'] != Role.STUDENT)
+
+
+def get_context(**kwargs):
+    '''Get the context for a request.
+
+    This function also aborts if permissions are violated. Course and
+    submission permissions (ie. the viewer must be an instructor or a student)
+    is always checked, if appropriate. The keyword arguments specify additional
+    permissions to check.
+
+    The viewer and role contexts makes it easier to see how a page renders for
+    another user. These two parameters overlap in semantics slightly. The
+    viewer parameter is used to render a page as though they are the user. This
+    is helpful for checking that a page renders correctly. However, an
+    appropriate viewer is not always available; for example, if an admin is the
+    sole instructor of a course, and wants to see the page as a non-admin
+    faculty, there is no viewer that could provide that. Hence the role
+    parameter, which can restrict the privileges of the user.
+
+    In sum:
+    * the user is for checking actual permissions
+    * the viewer is for acting as a specific person
+    * the role is for checking renders
+
+    Parameters:
+        login_required (bool): If the user must be logged in.
+    '''
+    # get URL parameters
+    url_args = request.args.to_dict()
+    context = {}
+    _set_user_context(context, url_args, **kwargs)
+    # check if login is required
+    if not kwargs.get('login_required', True):
+        return context
+    if not context['user']:
+        abort(401)
+    _set_viewer_context(context, url_args, **kwargs)
+    _set_course_context(context, url_args, **kwargs)
+    _set_instructor_context(context, url_args, **kwargs)
+    _set_role_context(context, url_args, **kwargs)
     return context
