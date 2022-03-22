5ddf90142006802631df62bd90e2be4dbc3bf067
Nicholas Novak <nicholasmnovak@gmail.com>
2022-03-22 15:05:22 -0700

    Implemented some more of the interface design

--- routes.py
+++ routes.py
@@ -6,82 +6,84 @@ from .forms import UserForm
 from .models import db, User, Course, Assignment, Question, QuestionFile
 from .dispatch import evaluate_submission
 
-blueprint = Blueprint(name='demograder', import_name='demograder')
+blueprint = Blueprint(name="demograder", import_name="demograder")
 
 
-@blueprint.route('/')
+@blueprint.route("/")
 def root():
     context = get_context(login_required=False)
-    if context['user']:
-        return redirect(url_for('demograder.home'))
-    return render_template('root.html')
+    if context["user"]:
+        return redirect(url_for("demograder.home"))
+    return render_template("root.html")
 
 
-@blueprint.route('/home')
+@blueprint.route("/home")
 def home():
     context = get_context()
-    if context['role'] >= Role.ADMIN:
-        context['users'] = User.query.all()
-        context['courses'] = Course.query.all()
-        context['assignments'] = Assignment.query.all()
-        context['questions'] = Question.query.all()
-        context['question_files'] = QuestionFile.query.all()
-    return render_template('home.html', **context)
+    if context["role"] >= Role.ADMIN:
+        context["users"] = User.query.all()
+        context["courses"] = Course.query.all()
+        context["assignments"] = Assignment.query.all()
+        context["questions"] = Question.query.all()
+        context["question_files"] = QuestionFile.query.all()
+    return render_template("home.html", **context)
 
-@blueprint.route('/overview')
+
+@blueprint.route("/overview")
 def admin_overview():
-    return render_template('overview.html')
+    return render_template("overview.html", **get_context())
 
-@blueprint.route('/user/<int:user_id>')
+
+@blueprint.route("/user/<int:user_id>")
 def user_view(user_id):
-    return f'{user_id=}' # TODO
+    return f"{user_id=}"  # TODO
 
 
-@blueprint.route('/course/<int:course_id>')
+@blueprint.route("/course/<int:course_id>")
 def course_view(course_id):
-    return f'{course_id=}' # TODO
+    return f"{course_id=}"  # TODO
 
 
-@blueprint.route('/question/<int:question_id>')
+@blueprint.route("/question/<int:question_id>")
 def question_view(question_id):
-    return f'{question_id=}' # TODO
+    return f"{question_id=}"  # TODO
 
 
-@blueprint.route('/submission/<int:submission_id>')
+@blueprint.route("/submission/<int:submission_id>")
 def submission_view(submission_id):
-    return f'{submission_id=}' # TODO
+    return f"{submission_id=}"  # TODO
 
 
-@blueprint.route('/download_submission/<int:submission_id>')
+@blueprint.route("/download_submission/<int:submission_id>")
 def download_submission(submission_id):
-    return f'{submission_id=}' # TODO
+    return f"{submission_id=}"  # TODO
 
 
-@blueprint.route('/result/<int:result_id>')
+@blueprint.route("/result/<int:result_id>")
 def result_view(result_id):
-    return f'{result_id=}' # TODO
+    return f"{result_id=}"  # TODO
 
 
-@blueprint.route('/download_result/<int:result_id>')
+@blueprint.route("/download_result/<int:result_id>")
 def download_result(result_id):
-    return f'{result_id=}' # TODO
+    return f"{result_id=}"  # TODO
 
 
-@blueprint.route('/file/<int:file>')
+@blueprint.route("/file/<int:file>")
 def file_view(file_id):
-    return f'{file_id=}' # TODO
+    return f"{file_id=}"  # TODO
 
 
-@blueprint.route('/download_file/<int:file>')
+@blueprint.route("/download_file/<int:file>")
 def download_file(file_id):
-    return f'{file_id=}' # TODO
+    return f"{file_id=}"  # TODO
 
 
 # FORMS
 
 
-@blueprint.route('/forms/user/', defaults={'user_id': None}, methods=('GET', 'POST'))
-@blueprint.route('/forms/user/<int:user_id>', methods=('GET', 'POST'))
+@blueprint.route("/forms/user/", defaults={"user_id": None}, methods=("GET", "POST"))
+@blueprint.route("/forms/user/<int:user_id>", methods=("GET", "POST"))
 def user_form(user_id):
     context = get_context(user=user_id)
     form = UserForm()
@@ -90,13 +92,13 @@ def user_form(user_id):
         if form.id.data:
             # if there is an ID, this is editing an existing User
             # make sure that the submitted ID is the same as the user ID
-            if not (context['user'].admin or int(form.id.data) == user_id):
+            if not (context["user"].admin or int(form.id.data) == user_id):
                 abort(403)
             user = User.query.get(form.id.data).first()
             user.preferred_name = form.preferred_name.data.strip()
             user.family_name = form.family_name.data.strip()
             # only an admin can change the email or the admin/faculty statuses
-            if context['user'].admin:
+            if context["user"].admin:
                 user.email = form.email.data.strip()
                 user.admin = form.admin.data
                 user.faculty = form.faculty.data
@@ -111,7 +113,7 @@ def user_form(user_id):
             )
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('demograder.home')) # FIXME
+        return redirect(url_for("demograder.home"))  # FIXME
     # the form is not being submitted
     if user_id is not None:
         # a user ID is provided; set the defaults to the user being edited
@@ -123,14 +125,16 @@ def user_form(user_id):
         form.admin.default = user.admin
         form.faculty.default = user.faculty
         # disable the email field for non-admins
-        if context['role'] < Role.ADMIN:
-            form.email.render_kw['disabled'] = ''
+        if context["role"] < Role.ADMIN:
+            form.email.render_kw["disabled"] = ""
         form.process()
-    return render_template('forms/user.html', form=form, **context)
+    return render_template("forms/user.html", form=form, **context)
 
 
-@blueprint.route('/forms/course/', defaults={'course_id': None}, methods=('GET', 'POST'))
-@blueprint.route('/forms/course/<int:course_id>', methods=('GET', 'POST'))
+@blueprint.route(
+    "/forms/course/", defaults={"course_id": None}, methods=("GET", "POST")
+)
+@blueprint.route("/forms/course/<int:course_id>", methods=("GET", "POST"))
 def course_form(course_id):
     pass
 
@@ -140,4 +144,4 @@ def course_form(course_id):
 
 @blueprint.errorhandler(401)
 def unauthorized_error(error):
-    return redirect(url_for('demograder.root'))
+    return redirect(url_for("demograder.root"))
