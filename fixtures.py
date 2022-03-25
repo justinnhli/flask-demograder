47823065acc4a2e288bfa25c21f5a7263b1d7026
Chris L <clinscott@oxy.edu>
2022-03-24 18:55:01 -0700

    Layout mockups
    Current designing mockup (so less CSS, more layout). 1 = course + submissions  indicated by color, 2 = assignment idea + submissions indiciated by class

--- fixtures.py
+++ fixtures.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sqlalchemy.exc import IntegrityError
 
 from .models import db, User, Course, Assignment, Question, QuestionFile
@@ -13,7 +14,37 @@ def install_fixtures(app):
                 admin=True,
                 faculty=True,
             )
+            user2 = User(
+                preferred_name='Christopher',
+                family_name='Linscott',
+                email='clinscott@oxy.edu',
+                admin=True,
+                faculty=False,
+            )
+            assignment1 = Assignment(
+                id=5328423,
+                course_id=34,
+                name='Homework 1',
+                due_date=datetime(2022, 3, 24, 23, 59, 59),
+            )
+            assignment2 = Assignment(
+                id=3435453,
+                course_id=34,
+                name='Homework 2',
+                due_date=datetime(2022, 3, 24, 23, 59, 59),
+            )
+            assignment3 = Assignment(
+                id=324342342,
+                course_id=34,
+                name='Homework 1',
+                due_date=datetime(2022, 3, 24, 23, 59, 59),
+            )
             db.session.add(user)
+            db.session.add(user2)
+            db.session.add(assignment1)
+            db.session.add(assignment2)
+            db.session.add(assignment3)
+
             db.session.commit()
         except IntegrityError:
             db.session.rollback()
