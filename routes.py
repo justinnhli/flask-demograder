a406e10fbfa4838bf2ae6d711cd0d0738fa68358
Chris L <clinscott@oxy.edu>
2022-04-28 03:07:50 -0700

    Initial home_student page design, CSS, route modifications and href link added to base.html

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, url_for, redirect, abort
+from pandas import concat
 from werkzeug.utils import secure_filename
 
-from .context import get_context, Role
+from .context import _set_student_context, get_context, Role
 from .forms import UserForm
 from .models import db, User, Course, Assignment, Question, QuestionFile
 from .dispatch import evaluate_submission
@@ -27,6 +28,10 @@ def home():
         context['questions'] = Question.query.all()
         context['question_files'] = QuestionFile.query.all()
         return render_template('home-admin.html', **context)
+    elif context['role'] >= Role.STUDENT:
+        context['courses'] = Course.query.all()
+        context['assignments'] = Assignment.query.all()
+        return render_template('home-student.html', **context)
     else:
         return render_template('home.html', **context)
 
@@ -38,7 +43,9 @@ def user_view(user_id):
 
 @blueprint.route('/course/<int:course_id>')
 def course_view(course_id):
-    return f'{course_id=}' # TODO
+    context = get_context()
+    context['course'] = Course.query.get(course_id)
+    return render_template('course-student.html', **context)
 
 
 @blueprint.route('/question/<int:question_id>')
