a3e64cd318f7fb4666f3079545aefc982fe5acd9
Justin Li <justinnhli@gmail.com>
2024-10-31 15:53:11 -0700

    check ports in run script

--- run-app.sh
+++ run-app.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-. demograder/secrets && gunicorn --bind 0.0.0.0:5000 'demograder:create_app()'
+if lsof -i:5000 >/dev/null 2>&1; then
+    echo 'a program is using port 5000; output of `lsof -i:5000`:'
+    lsof -i:5000
+elif ! systemctl status httpd | grep inactive >/dev/null 2>&1; then
+    echo 'httpd service is running, and likely using port 5000'
+    echo 'kill httpd as root with `systemctl stop httpd` first and try again'
+else
+    . demograder/secrets && gunicorn --bind 0.0.0.0:5000 'demograder:create_app()'
+fi
